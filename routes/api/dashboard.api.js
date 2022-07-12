444d7b1451facea43345ef761fd268d57151dc31
N0tail12 <N0tail12>
2022-07-13 00:13:09 +0700

    feat: add dashboard feature

--- routes/api/dashboard.api.js
+++ routes/api/dashboard.api.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const DashboardRepository = require("../../controller/api/dashboard.controller");
 
 router.route("/dashboard/all-item").get(DashboardRepository.GetAllItem);
+router.route("/dashboard/item-by-id").get(DashboardRepository.GetItemByID);
 
 module.exports = router;
